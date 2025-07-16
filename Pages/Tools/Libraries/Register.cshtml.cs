f5d3d1ea0069e4e1e5d646294d7951c8fb5aa524
Chinatsu Kano <wishinggem15710@gmail.com>
2025-07-16 19:20:37 +0100

    Added email to register form and json handeling

--- Pages/Tools/Libraries/Register.cshtml.cs
+++ Pages/Tools/Libraries/Register.cshtml.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.RegularExpressions;
 
 namespace PersonalUniversalSite.Pages.Tools.Libraries
 {
     public class RegisterModel : PageModel
     {
-        private readonly string accountsPath = Path.Combine("wwwroot", "Accounts", "Accounts.json");
+        private readonly string accountsPath = Path.Combine("wwwroot", "Libraries", "Manga", "Accounts.json");
         private readonly string librariesPath = Path.Combine("wwwroot", "Libraries", "Manga", "Libraries.json");
         private readonly string accToLibPath = Path.Combine("wwwroot", "Libraries", "Manga", "accToLib.json");
 
@@ -17,7 +18,7 @@ namespace PersonalUniversalSite.Pages.Tools.Libraries
 
         }
 
-        public IActionResult OnPost(string username, string password)
+        public IActionResult OnPost(string username, string password, string email)
         {
             JsonFileInitializer.EnsureAllLibraryFilesExist(accountsPath, accToLibPath, librariesPath);
 
@@ -32,6 +33,18 @@ namespace PersonalUniversalSite.Pages.Tools.Libraries
                 return Page();
             }
 
+            if (accounts.Any(a => a.email == email))
+            {
+                ErrorMessage = "That email is already in use.";
+                return Page();
+            }
+
+            if (!CheckValidEmailFormat(email))
+            {
+                ErrorMessage = "That email is in an incorrect format.";
+                return Page();
+            }
+
             // Generate new account
             string newID = Guid.NewGuid().ToString();
             var hasher = new PasswordHandler();
@@ -40,6 +53,7 @@ namespace PersonalUniversalSite.Pages.Tools.Libraries
             var newAccount = new Account
             {
                 ID = newID,
+                email = email,
                 userName = username,
                 password = hashed,
                 salt = salt
@@ -61,6 +75,19 @@ namespace PersonalUniversalSite.Pages.Tools.Libraries
             HttpContext.Session.SetString("loggedInAccountID", newID);
 
             return RedirectToPage("/Tools/Libraries/MangaLibrary");
+        }
+
+        private bool CheckValidEmailFormat(string email)
+        {
+            Regex emailRegex = new Regex(@"(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])");
+            if (emailRegex.IsMatch(email))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
